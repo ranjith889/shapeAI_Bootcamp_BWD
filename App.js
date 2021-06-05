8a5e4e80add25873afbe8870e3ff541ec233f1da
ranjith889 <85386110+ranjith889@users.noreply.github.com>
2021-06-05 12:40:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM fromn"react-dom";
+import app from"./components/app";
+
+ReactDOM.render(
+  <div>
+    <App />
+    </div>,
+    document.getElementById("root")
+);
